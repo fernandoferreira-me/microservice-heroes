7dd4db534d5e57878106625937b4d578289c9e7e
Fernando Ferreira <fernandogferreira@gmail.com>
2019-09-03 18:32:38 -0300

    Adding Swagger Docs.

--- heroes/heroes.js
+++ heroes/heroes.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const router = express.Router();
 const path = require('path');
 const bodyParser = require('body-parser');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 
 const port = process.argv.slice(2)[0];
 const app = express();
@@ -9,6 +14,10 @@ const app = express();
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/v1', router);
 
 const powers = [
   { id: 1, name: 'flying' },
